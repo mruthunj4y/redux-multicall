adab3849761805f8e326050151117594018b0598
mruthunj4y <mruthunjay.parmar@syvora.com>
2025-07-15 16:57:57 +0530

    fix: removed console.log

--- src/updater.tsx
+++ src/updater.tsx
@@ -28,7 +28,6 @@ async function fetchChunk(
   blockNumber: number,
   isDebug?: boolean
 ): Promise<{ success: boolean; returnData: string }[]> {
-  console.debug('Fetching chunk', chunk, blockNumber)
   try {
     const { returnData } = await multicall.callStatic.multicall(
       chunk.map((obj) => ({
